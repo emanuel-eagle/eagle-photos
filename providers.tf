b9eac5ca75b0496491a37bf28f1a5e682428220a
Emanuel Eagle <emanueleagle@Emanuels-MacBook.local>
2026-08-06 22:06:58 -0400

    need to add backend jeezus

--- providers.tf
+++ providers.tf
@@ -10,3 +10,13 @@ terraform {
 provider "aws" {
   # Configuration options
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-file-eagle-photos"
+    key            = "environments/production/terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
+    use_lockfile   = true # Enables native S3 locking (requires Terraform 1.10+)
+  }
+}
